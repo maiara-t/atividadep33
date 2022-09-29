fa604f970272b4ae5682257ca2436be0d3e117eb
maiara-t <maiaratsm2@gmail.com>
2022-09-29 11:09:25 -0300

    comit comit2 comit 3

--- src/GerenciaEmprestimo.java
+++ src/GerenciaEmprestimo.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
+
 public class GerenciaEmprestimo {
 
+GerenciaEmprestimo emprestimo = new GerenciaEmprestimo();
+
+    private boolean cadrastroEmprestimoExistente(String id, String cpf) {
+        if (getId(id) == null && getCpf) {
+            return false;
+        }
+        return true;
+    }
+    public void cadastroDeEmprestimo (Usuario, Livro) {
+        if (!cadastroEmprestimoExistente(cpf, id)) {
+            cadastros.add(new Usuario(cpf, id) {
+            });
+        }
+    }
 
 }
